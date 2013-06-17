4979e12bee944415fbf43a34f7e3b63f4edb74dc
wjboogerd <wjboogerd@eledge.net>
2013-06-17 16:05:55 +0200

    Added toStringArray method

--- src/net/eledge/android/toolkit/StringArrayUtils.java
+++ src/net/eledge/android/toolkit/StringArrayUtils.java
@@ -7,6 +7,10 @@ import android.util.SparseArray;
 
 public class StringArrayUtils {
 
+	public static String[] toStringArray(List<String> list) {
+	    return list.toArray(new String[list.size()]);
+	}
+	
 	public static String[] toStringArray(SparseArray<List<String>> sparseArray) {
 		List<String> list = new ArrayList<String>();
 		if ( (sparseArray != null) && (sparseArray.size() > 0)) {
